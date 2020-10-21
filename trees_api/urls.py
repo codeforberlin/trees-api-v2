626005fb0ccb07f6ff3e1ef4fd3fd8cf47550567
Jochen Klar <mail@jochenklar.de>
2020-10-21 10:39:31 +0200

    Fix urls for newer drf

--- trees_api/urls.py
+++ trees_api/urls.py
@@ -1,19 +1,13 @@
 from django.urls import include, path
-
 from rest_framework import routers
 
-from .viewsets import (
-    TreeViewSet,
-    SpeciesViewSet,
-    GenusViewSet,
-    BoroughViewSet
-)
+from .viewsets import BoroughViewSet, GenusViewSet, SpeciesViewSet, TreeViewSet
 
 router = routers.DefaultRouter()
-router.register(r'trees', TreeViewSet, base_name='tree')
-router.register(r'species', SpeciesViewSet, base_name='species')
-router.register(r'genera', GenusViewSet, base_name='genus')
-router.register(r'boroughs', BoroughViewSet, base_name='borough')
+router.register(r'trees', TreeViewSet, basename='tree')
+router.register(r'species', SpeciesViewSet, basename='species')
+router.register(r'genera', GenusViewSet, basename='genus')
+router.register(r'boroughs', BoroughViewSet, basename='borough')
 
 urlpatterns = [
     path('', include(router.urls)),
